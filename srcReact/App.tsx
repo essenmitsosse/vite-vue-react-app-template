48316f58c928c421562aaddaa5bbd2b9a35af8ac
Marcus Blättermann <marcus@essenmitsosse.de>
2022-02-21 23:27:16 +0100

    Merge apps

--- srcReact/App.tsx
+++ srcReact/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo App-logo-react" alt="logo" />
         <h1>{message}</h1>
       </header>
     </div>

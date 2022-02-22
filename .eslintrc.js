7d326f2958410ebc453d250742551bc7c4aa6973
Marcus Blättermann <marcus@essenmitsosse.de>
2022-02-22 22:43:18 +0100

    Fix eslint and typescript for src folder

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+  overrides: [
+    // This makes sure typescript rules are only used for typescript files
+    // https://stackoverflow.com/questions/58510287/parseroptions-project-has-been-set-for-typescript-eslint-parser
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+  ],
+};
